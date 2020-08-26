617d54a0e947fca3bbe10563da6d68f7c1fef96a
Supansa Tantulset <s6201012620236@email.kmutnb.ac.th>
2020-08-26 22:34:31 +0700

    Add container for display number

--- Calculator.java
+++ Calculator.java
@@ -1,13 +1,35 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Calculator {
 
     public static void main(String[] args) {
+        showGUI();
+
+    }
+
+    static void showGUI() {
+
+        // set Frame
         JFrame frame = new JFrame();  // JFrame is class for create form(window)
         frame.setSize(400,430); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // define event of object CreateFrame when click close on titlebar 
-        frame.setVisible(true);  // for display on screen 
-        frame.setTitle("Calculator"); //set title bar
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  // define event of object CreateFrame when click close on titlebar 
+        frame.setTitle("Calculator");  //set title bar
+        frame.getContentPane().setBackground(new Color(15,76,129));
+    
+        // set lable of display number
+        JPanel panel1 = new JPanel(); // create panel for container component
+        panel1.setLayout(null);
+        panel1.setBackground(new Color(15,76,129));
+        frame.add(panel1);
+    
+        JTextField displayNumber = new JTextField(); // rect for display number
+        displayNumber.setBounds(15,15,355,82);
+        panel1.add(displayNumber);
+    
+        // for display on screen
+        frame.setVisible(true);
+
     }
 
 }
